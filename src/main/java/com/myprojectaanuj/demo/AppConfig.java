84da27583ca7fd763c7085d3a1b3558bc4ccaedb
Aanuj Jain <aanujjain750@gmail.com>
2026-03-20 11:37:47 +0530

    APIProject1.0 - Creation

---
+++ src/main/java/com/myprojectaanuj/demo/AppConfig.java
@@ -0,0 +1,12 @@
+package com.myprojectaanuj.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    PaymentService paymentService(){
+        return new PaymentService();
+    }
+}
